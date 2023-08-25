8df4533786e568ffd0e420e38261696696d0a898
junbeompark <a01077348062@gmail.com>
2023-08-25 19:21:42 +0900

    댓글 create 기능 추가

--- articles/views.py
+++ articles/views.py
@@ -52,3 +52,27 @@ def detail(request, id):
     }
     
     return render(request, 'detail.html', context)
+
+def comment_create(request, article_id):
+    # 사용자가 입력한 정보를 form에 입력
+    comment_form = CommentForm(request.POST)
+    
+    # 유효성 검사
+    if comment_form.is_valid():
+        # form을 저장 => 추가로 넣어야 하는 데이터를 넣기 위해 저장을 맘춤
+        comment = comment_form.save(commit=False)
+        
+        # 첫 번째 방법
+        # article_id을 기준으로 article obj를 가져와서
+        # article_id = Article.objects.get(id=article_id)
+        # article 컬럼에 추가
+        # comment.article = article
+        
+        # 두 번째 방법
+        comment.article_id = article_id
+        
+        # 이 후, 저장
+        comment.save()
+        
+        return redirect('articles:detail', id=article_id)
+    
